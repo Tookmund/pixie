1b1bc7e40619ff074310acd0121045301ce5af7e
Jacob Adams <jacob@tookmund.com>
2019-06-23 23:58:31 -0400

    Setup efi64 and efi32 correctly with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 ln -s $PWD/bios.cfg bios/pxelinux.cfg
 ln -s /usr/lib/PXELINUX/pxelinux.0 bios/
+
+ln -s $PWD/efi64.cfg efi64/pxelinux.cfg
+ln -s /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi efi64/
+
+ln -s $PWD/efi32.cfg efi32/pxelinux.cfg
+ln -s /usr/lib/SYSLINUX.EFI/efi32/syslinux.efi efi32/
