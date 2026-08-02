6c89a1b10fa6d2203b82d2e7daf1045afd9c7b1a
Jacob Adams <jacob@tookmund.com>
2026-08-02 14:13:17 -0400

    Make setup script and ansible role resilient to restarts

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -s $PWD/bios.cfg bios/pxelinux.cfg
-ln -s /usr/lib/PXELINUX/pxelinux.0 bios/
+ln -sf $PWD/bios.cfg bios/pxelinux.cfg
+ln -sf /usr/lib/PXELINUX/pxelinux.0 bios/
 
-ln -s $PWD/efi64.cfg efi64/pxelinux.cfg
-ln -s /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi efi64/
+ln -sf $PWD/efi64.cfg efi64/pxelinux.cfg
+ln -sf /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi efi64/
 
-ln -s $PWD/efi32.cfg efi32/pxelinux.cfg
-ln -s /usr/lib/SYSLINUX.EFI/efi32/syslinux.efi efi32/
+ln -sf $PWD/efi32.cfg efi32/pxelinux.cfg
+ln -sf /usr/lib/SYSLINUX.EFI/efi32/syslinux.efi efi32/
